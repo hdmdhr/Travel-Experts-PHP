6d4bfd8cd3a4647f1b921a12e8665810a7bbb90c
DongMing Hu <821312@ACDM.DS.SAIT.CA>
2019-02-11 08:17:32 -0700

    Final php submission status.

--- php/header.php
+++ php/header.php
@@ -1,3 +1,12 @@
+<!-- **************************
+*
+* Author: DongMing Hu
+* Date: Feb. 11, 2019
+* Course: CPRG 210 PHP
+* Description: html body header include welcome message and navigation menu
+*
+************************** -->
+
 <header>
 
   <div class="logo-line">
@@ -27,30 +36,30 @@
         }
         // TODO: if logged in, say the name in session['user-id']
         if (isset($_SESSION['user-id'])) {
-          echo "<em>".$_SESSION['user-id']."</em>.<a href='logout.php' ><button class='btn btn-outline-secondary ml-4'>Logout</button></a></h3>";
+          echo "Agent <em>".$_SESSION['user-id']."</em>.<a href='logout.php' ><button class='btn btn-outline-secondary ml-4'>Logout</button></a></h3>";
         } else {
-          echo "Dude.<a href='login.php' ><button class='btn btn-outline-primary ml-4'>Login</button></a></h3>";
+          echo "Friend.<a href='login.php' ><button class='btn btn-outline-primary ml-4'>Login</button></a></h3>";
         }
        ?>
   </div>
 
   <nav class="nav-bar row">
-        <div class="nav-tab home col-sm-4 col-md-2">
+        <div class="nav-tab home col-sm-6 col-md-4 col-lg-2">
           <a href="index.php" target="_blank"><img src="img/home.png" alt="Home">Home</a>
         </div>
-        <div class="nav-tab contact col-sm-4 col-md-2">
+        <div class="nav-tab contact col-sm-6 col-md-4 col-lg-2">
           <a href="contact.php" target="_blank"><img src="img/contacts.png" alt="Contact Us">Contact Us</a>
         </div>
-        <div class="nav-tab register col-sm-4 col-md-2">
+        <div class="nav-tab register col-sm-6 col-md-4 col-lg-2">
           <a href="register.php" target="_blank"><img src="img/register.png" alt="Register Now">Register</a>
         </div>
-        <div class="nav-tab spots col-sm-4 col-md-2">
+        <div class="nav-tab spots col-sm-6 col-md-4 col-lg-2">
           <a href="spots.php" target="_blank"><img src="img/chillies.png" alt="hot spots">Famous Spots</a>
         </div>
-        <div class="nav-tab add-agent col-sm-4 col-md-2">
+        <div class="nav-tab add-agent col-sm-6 col-md-4 col-lg-2">
           <a href="new-agent.php" target="_blank"><img src="img/add.png" alt="fun">Add Agent</a>
         </div>
-        <div class="nav-tab links col-sm-4 col-md-2">
+        <div class="nav-tab links col-sm-6 col-md-4 col-lg-2">
           <a href="links.php" target="_blank"><img src="img/computer.png" alt="tech">Links</a>
         </div>
   </nav>
--- php/menu.php
+++ php/menu.php
@@ -1,17 +1,29 @@
-<nav class="nav-bar">
-  <div class="nav-tab">
-    <a href="contact.php" target="_blank"><img src="img/contacts.png" alt="Contact Us">Contact Us</a>
-  </div>
-  <div class="nav-tab">
-    <a href="register.php" target="_blank"><img src="img/register.png" alt="Register Now">Register</a>
-  </div>
-  <div class="nav-tab">
-    <a href="spots.php" target="_blank"><img src="img/chillies.png" alt="hot spots">Famous Spots</a>
-  </div>
-  <div class="nav-tab">
-    <a href="https://tenor.com/search/cute-kitten-gifs" target="_blank"><img src="img/avatar.gif" alt="fun">Fun</a>
-  </div>
-  <div class="nav-tab">
-    <a href="links.php" target="_blank"><img src="img/computer.png" alt="tech">Links Page</a>
-  </div>
+<!-- **************************
+*
+* Author: DongMing Hu
+* Date: Feb. 11, 2019
+* Course: CPRG 210 PHP
+* Description: html element navigation menu
+*
+************************** -->
+
+<nav class="nav-bar row">
+      <div class="nav-tab home col-sm-6 col-md-4 col-lg-2">
+        <a href="index.php" target="_blank"><img src="img/home.png" alt="Home">Home</a>
+      </div>
+      <div class="nav-tab contact col-sm-6 col-md-4 col-lg-2">
+        <a href="contact.php" target="_blank"><img src="img/contacts.png" alt="Contact Us">Contact Us</a>
+      </div>
+      <div class="nav-tab register col-sm-6 col-md-4 col-lg-2">
+        <a href="register.php" target="_blank"><img src="img/register.png" alt="Register Now">Register</a>
+      </div>
+      <div class="nav-tab spots col-sm-6 col-md-4 col-lg-2">
+        <a href="spots.php" target="_blank"><img src="img/chillies.png" alt="hot spots">Famous Spots</a>
+      </div>
+      <div class="nav-tab add-agent col-sm-6 col-md-4 col-lg-2">
+        <a href="new-agent.php" target="_blank"><img src="img/add.png" alt="fun">Add Agent</a>
+      </div>
+      <div class="nav-tab links col-sm-6 col-md-4 col-lg-2">
+        <a href="links.php" target="_blank"><img src="img/computer.png" alt="tech">Links</a>
+      </div>
 </nav>
