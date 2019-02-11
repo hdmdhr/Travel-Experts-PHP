debfb8a282e05be85da19ac301371e69936eebad
paruhang <liveless44@live.com>
2019-02-11 15:44:17 -0700

    Save changes.

--- login.php
+++ login.php
@@ -29,7 +29,10 @@ if(session_id() == '' || !isset($_SESSION)) {
       if ($_POST['Password'] === $array[$ID]) {  // pin match
         // save user-id to session, head to agent entry page
         $_SESSION['user-id'] = $_POST['UserId'];
-        header("Location: http://localhost/CPRG-210-OSD-Assignment/new-agent.php");
+        // agents and users could use same login page but send you to different page.
+          header("Location: http://localhost/ThreadProj/customerpage.php");
+
+        //header("Location: http://localhost/CPRG-210-OSD-Assignment/new-agent.php");
       } else { echo "<h2 class='alert alert-danger' role='alert'>Password or User ID do NOT match.</h2>"; }
     } else { echo "<h2 class='alert alert-danger' role='alert'>User ID or Password do NOT match.</h2>"; }
   } else { echo "No post received."; }
@@ -57,10 +60,14 @@ if(session_id() == '' || !isset($_SESSION)) {
   <?php include_once('php/header.php') ?>
 
   <form class="form-signin mt-5" method="post" action="#">
+
+
     <a href="index.php" target="_blank">
       <img class="mb-2" src="img/balloon.png" alt="logo" width="72" height="72">
     </a>
+
     <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+
     <label for="user-id" class="sr-only">User ID</label>
     <input type="text" id="user-id" class="form-control" name="UserId" placeholder="Email address" required autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
